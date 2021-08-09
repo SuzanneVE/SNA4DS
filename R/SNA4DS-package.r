1feafc3f047185b84cb853f02e6a457f06d9d604
SNAnalyst <r.t.a.j.leenders@tilburguniversity.edu>
2021-08-09 20:46:45 +0200

    ?SNA4DS enabled

---
+++ R/SNA4DS-package.r
@@ -0,0 +1,21 @@
+
+
+
+# The following block is used by usethis to automatically manage
+# roxygen namespace tags. Modify with care!
+## usethis namespace: start
+## usethis namespace: end
+NULL
+
+
+
+#' SNA4DS
+#'
+#' Package supporting the SNA4DS course
+#'
+#' Functions, datasets, and tutorials, useful for the SNA4DS course.
+#'
+#' @name SNA4DS
+#' @keywords package
+#' @docType package
+NULL
