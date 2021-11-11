e7840ac66b99f48a1286b6fad94ee21c49d88c8a
Zucca <c.zucca@jads.nl>
2021-11-11 11:11:28 +0100

    fixing typo in GOG.plot

--- R/GOF.plot.R
+++ R/GOF.plot.R
@@ -2,15 +2,15 @@
 #'
 #'Plots the Goodness of fit for the output of a model of clas `ergm`
 #'
-#' This function processes the output of unipartite Exponential Random Graphs Models only. 
-#' 
-#' It does not process bipartite ERGMs model objects nor other types of Exponential random graph models object. 
-#' 
+#' This function processes the output of unipartite Exponential Random Graphs Models only.
+#'
+#' It does not process bipartite ERGMs model objects nor other types of Exponential random graph models object.
+#'
 
 #' @param m A model object
 #'
 #' @return Four Plots displaying the Goodness of Fit
-#' @export GoF.plot
+#' @export GOF.plot
 #'
 #' @examples
 #' \dontrun{
@@ -22,10 +22,10 @@
 #' GOF.plot(m)
 #' }
 GOF.plot <- function(m) {
-  
+
   summary(m)
-  
+
   g <- ergm::gof(m1)
-  
+
   ergm:::plot.gof(g)
 }
